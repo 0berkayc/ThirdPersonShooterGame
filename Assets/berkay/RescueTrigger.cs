42c571a65859d80d1aa42dec6547e7d72aaf4ce2
ceren <cerenyildiz377@gmail.com>
2025-11-07 15:48:22 +0300

    ending ekranı ve scripti eklendi

---
+++ Assets/berkay/RescueTrigger.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class RescueTrigger : MonoBehaviour
+{
+    private bool playerNearby = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            Debug.Log("Player kafesin önünde, E'ye basabilirsin.");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerNearby && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            GoToVictoryScene();
+        }
+    }
+
+    void GoToVictoryScene()
+    {
+        Debug.Log("Kurtarma tamamlandý! VictoryScreen sahnesine geçiliyor...");
+        SceneManager.LoadScene("VictoryScreen");
+    }
+}
